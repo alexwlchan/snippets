c652eb9abf14da78aecb1f9f863bc7ccc1cfedfe
Alex Chan <alex@alexwlchan.net>
2025-02-10 17:10:58 +0000

    Add type hints; algo doesn't need to be configurable

--- expansions/create_hash.py
+++ expansions/create_hash.py
@@ -1,14 +1,15 @@
 import hashlib
+from pathlib import Path
 
 
-def create_hash(path, *, algorithm=hashlib.sha256):
+def create_hash(path: Path) -> "hashlib._Hash":
     """
-    Returns the hex checksum of the given path.
+    Returns the checksum of the given path.
     """
-    h = algorithm()
+    h = hashlib.sha256()
 
     with open(path, "rb") as infile:
         while chunk := infile.read(8192):
             h.update(chunk)
 
-    return h.hexdigest()
+    return h
